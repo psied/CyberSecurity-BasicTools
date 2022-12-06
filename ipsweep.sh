e199fdef22f71c1d0af4b8d0562606e783fe821e
Syed Masood I <99554395+psied@users.noreply.github.com>
2022-12-06 21:28:59 +0530

    Add files via upload
    This is a simple bash script which automated sweeping of IP Addresses and saved them to a text file.

---
+++ ipsweep.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$1" == " " ]
+then
+echo "Try: ./ipsweep.sh 192.168.4"
+
+else
+for ip in `seq 1 254`; do
+ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" >> sweepedips.txt & 
+done
+fi
